a758899152823bc1aafcab845d91845349cbad3d
Teagan Clayton <clay0063@algonquinlive.com>
2023-10-30 19:25:37 -0400

    Organized code, prints each as a sentence

--- bin/hybrid_4.dart
+++ bin/hybrid_4.dart
@@ -1,30 +1,28 @@
-import 'package:hybrid_4/hybrid_4.dart' as hybrid_4;
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'package:hybrid_4/hybrid_4.dart' as utils;
 
 
 void main(List<String> arguments) async {
+
   var base = 'random-data-api.com';
   var path = '/api/v2/users';
-  var params = {'size': '5'};
+  var params = {'size': '10'};
+  
   try {
     var uri = Uri.https(base, path, params);
     var response = await http.get(uri);
-    //"read" is used to read the response as a string. "get" is used to store the entire response
-    var json = convert.jsonDecode(response.body) as List<dynamic>;
-
-    List<Map<String, dynamic>> extractedData = [];
-
-    for (var item in json) {
-      var extractedItem = {'id': item['id'], 'username': item['username']};
-
-      extractedData.add(extractedItem);
-    }
     
-    for (var item in extractedData) {
-      print(item);
-      print('');
+    var json = convert.jsonDecode(response.body) as List<dynamic>;
+    var userList = utils.getNameAndID(json); //convert to string string, and extract only data needed
+
+    for (var item in userList) {
+      var uid = item['uid'];
+      var firstName = item['first_name'];
+      var lastName = item['last_name'];
+      print('$firstName $lastName, id #$uid\n');
     }
+
   } catch (err){
     print('Failed http request: $err');
   }
--- lib/hybrid_4.dart
+++ lib/hybrid_4.dart
@@ -1,3 +1,16 @@
-int calculate() {
-  return 6 * 7;
+List<Map<String, String>> getNameAndID(List<dynamic> dataList) {
+
+  List<Map<String, String>> extractedData = [];
+  
+  for (var item in dataList) {
+    var extractedItem = Map<String, String>.from({
+      'uid': item['uid'].toString(), 
+      'first_name': item['first_name'], 
+      'last_name': item['last_name']
+      });
+
+    extractedData.add(extractedItem);
+  }
+
+  return extractedData;
 }
