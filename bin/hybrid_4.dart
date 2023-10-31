df07552e1c4cc30ec97880c56ab52cae978df276
Teagan Clayton <clay0063@algonquinlive.com>
2023-10-30 20:48:41 -0400

    Change how stringification is done & output displays

--- bin/hybrid_4.dart
+++ bin/hybrid_4.dart
@@ -2,16 +2,17 @@ import 'package:hybrid_4/hybrid_4.dart' as utils;
 
 void main(List<String> arguments) async {
 
-  var base = 'random-data-api.com';
-  var path = '/api/v2/users';
-  var params = {'size': '10'};
+  String base = 'random-data-api.com';
+  String path = '/api/v2/users';
+  Map<String, String> params = {'size': '10'};
 
-  var json = await utils.fetchData(base, path, params);
-  var userList = utils.getNameAndID(json);
+  List<Map<String, String>> jsonList = await utils.fetchData(base, path, params);
 
-  for (var item in userList) {
-    var { 'uid': uid, 'first_name': firstName, 'last_name': lastName } = item; //for map destructuring
-    print('$firstName $lastName, id #$uid\n');
+  for (var item in jsonList) {
+    String uid = item['uid'] ?? 'NO UID';
+    String firstName = item['first_name'] ?? 'NO NAME';
+    String lastName = item['last_name'] ?? 'NO SURNAME';
+    print('ID #$uid\t$firstName $lastName\n');
   }
   
 }
--- lib/hybrid_4.dart
+++ lib/hybrid_4.dart
@@ -1,37 +1,35 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
-List<Map<String, String>> getNameAndID(List<dynamic> dataList) {
-  //asserting that the fetch is a List<dynamic> prevents it from fetching only 0 or 1 items, meaning checking here is unnecessary.
-  List<Map<String, String>> extractedData = [];
-  
-  for (var item in dataList) {
-    var extractedItem = Map<String, String>.from({
-      'uid': item['uid'].toString(), 
-      'first_name': item['first_name'], 
-      'last_name': item['last_name']
-      });
-
-    extractedData.add(extractedItem);
-  }
-
-  return extractedData;
-}
-
-Future<List<dynamic>> fetchData(String base, String path, Map<String, String> params) async {
+Future<List<Map<String, String>>> fetchData(String base, String path, Map<String, String> params) async {
   try {
     var uri = Uri.https(base, path, params);
     var response = await http.get(uri);
-    var json = convert.jsonDecode(response.body) as List<dynamic>;
-    return json;
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body) as List<dynamic>;
+      return convertToJSON(json);
+    } else {
+      String error = response.statusCode.toString();
+      print('Error fetching data: status code $error');
+      return List<Map<String, String>>.empty();
+    }
   } catch (err) {
     print('Error fetching data: $err');
-    return List<dynamic>.empty();
+    return List<Map<String, String>>.empty();
     //if try-catch is done here, then any thrown error will stop here, making wrapping it in main as well unnecessary
   }
-}
-
 
   //can also make "params" optional by wrapping it in a [?] in the func param list
   //and then Uri.https will automatically not include a null params
-  //need params here though
+}
+
+List<Map<String, String>> convertToJSON(List<dynamic> jsonList) {
+  //asserting that the fetch is a List<dynamic> prevents it from fetching only 0 or 1 items
+  return List<Map<String, String>>.from(jsonList.map((dynamic item) {
+    Map<String, String> newMap = {};
+    item.forEach((key, value) {
+      newMap[key] = value.toString();
+    });
+    return newMap;
+  }));
+}
